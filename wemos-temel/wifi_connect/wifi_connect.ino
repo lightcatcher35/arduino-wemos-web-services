ec6709a2077daa10848e5ad651fa6664338dd3d0
ilker özer <ilker@Kullanici-MacBook-Pro.local>
2020-04-23 22:17:53 +0300

    ADD wemos-wifi

---
+++ wemos-temel/wifi_connect/wifi_connect.ino
@@ -0,0 +1,16 @@
+
+#include <ESP8266WiFi.h>
+
+
+const char* ssid     = "paradox35";
+const char* password = "SimonSchama35";
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
